0529d23e5b9aa649aa4ead7c27d1ef2c47917254
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-08-17 16:10:36 +0000

    added some stuff

---
+++ Homework/One/notes.js
@@ -0,0 +1,13 @@
+/*
+A typical person has an average heart rate of 68.0 beats/min. Calculate the given questions.How many beats does she have in 4.0 years?
+How many beats in 4.00 years?
+And finally, how many beats in 4.000 years?
+Pay close attention to significant figures
+in this question.
+*/
+
+const heartbeats = 68.0;
+
+const years_one = 4.0;
+
+console.log(heartbeats * years_one);
